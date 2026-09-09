972793e71673663155525968b60e26f3a58ed343
“David” <davidmendoza@6423@gmail.com>
2026-09-09 00:15:33 -0700

    feat(python-cicd): add linting and format checks

--- src/simple_http_checker/cli.py
+++ src/simple_http_checker/cli.py
@@ -1,20 +1,28 @@
 import logging
 import click
 from .checker import check_urls
+from typing import Collection
 
 logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s] %(levelname)-8s %(name)s: %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S"
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 logger = logging.getLogger(__name__)
 
+
 @click.command()
 @click.argument("urls", nargs=-1)
-@click.option("--timeout", default = 5, help = "Timeout in seconds for each request.")
-@click.option("--verbose", "-v", is_flag = True, help = "Enable debug logging.")
-def main(urls, timeout, verbose):
+@click.option(
+    "--timeout",
+    default=5,
+    help="Timeout in seconds for each request.",
+)
+@click.option(
+    "--verbose", "-v", is_flag=True, help="Enable debug logging."
+)
+def main(urls: Collection[str], timeout: int, verbose: bool):
     if verbose:
         logging.getLogger().setLevel(logging.DEBUG)
         logger.debug("Verbose logging enabled.")
@@ -39,5 +47,4 @@ def main(urls, timeout, verbose):
         else:
             fg_color = "red"
 
-        click.secho(f"{url:<40} -> {status}", fg = fg_color)
-    
+        click.secho(f"{url:<40} -> {status}", fg=fg_color)
